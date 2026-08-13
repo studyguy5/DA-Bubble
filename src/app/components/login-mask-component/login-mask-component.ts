8feaa569f18a9c98fcc9516513dbafd81fb4ad22
Leonhard Siegele <leonhard.siegele39@gmail.com>
2026-08-13 15:23:38 +0200

    styled default Login Mask

--- src/app/components/login-mask-component/login-mask-component.ts
+++ src/app/components/login-mask-component/login-mask-component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login-mask-component',
@@ -6,4 +7,10 @@ import { Component } from '@angular/core';
   templateUrl: './login-mask-component.html',
   styleUrl: './login-mask-component.scss',
 })
-export class LoginMaskComponent {}
+export class LoginMaskComponent {
+
+  loginGroup = new FormGroup({
+    email: new FormControl('I` am back'),
+    password: new FormControl(''),
+  });
+}
