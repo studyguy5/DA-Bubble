e5af6c7bba819f8525f025ed28f6ffd16a44d665
Leonhard Siegele <leonhard.siegele39@gmail.com>
2026-08-13 02:31:14 +0200

    implemented login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginRegisterPageComponent } from './components/login-register-page-component/login-register-page-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: LoginRegisterPageComponent
+    }
+];
--- src/app/components/login-register-page-component/login-register-page-component.ts
+++ src/app/components/login-register-page-component/login-register-page-component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, DOCUMENT, Inject, signal } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginMaskComponent } from '../login-mask-component/login-mask-component';
 
+type maskType = {
+  loginMask: string;
+  registerMask: string;
+  forgotPasswordMask: string;
+  avatarMask: string;
+}
 @Component({
   selector: 'app-login-register-page-component',
-  imports: [],
+  imports: [LoginMaskComponent],
+  standalone: true,
   templateUrl: './login-register-page-component.html',
   styleUrl: './login-register-page-component.scss',
 })
-export class LoginRegisterPageComponent {}
+export class LoginRegisterPageComponent {
+
+  
+
+  readonly activeMask = signal('loginMask');
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    this.addBodyClass();
+  }
+
+  OnInit(){
+    this.activeMask.set('loginMask');
+  }
+
+  addBodyClass(){
+    this.document.body.classList.add('loginRegisterPage');
+  }  
+}
